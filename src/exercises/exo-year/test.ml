1748e099b83307f7c038f2e8a5cb519db92a67f6
Erik Martin-Dorel <erik.martin-dorel@irit.fr>
2022-10-11 15:38:27 +0200

    feat: Improve exo-year/test.ml

--- src/exercises/exo-year/test.ml
+++ src/exercises/exo-year/test.ml
@@ -2,14 +2,21 @@ open Test_lib
 open Learnocaml_report
 
 let q_nbjour =
-  let prot = [%funty: int -> int -> int] in
   let mois = [1; 2; 3; 4; 5; 6; 7; 8; 9; 10; 11; 12] in
   let ans = [1000; 1100; 1900; 1904; 2000; 2004; 2010; 2018; 2020; 2100] in
-  test_function_against_solution ~gen:0 prot "nbjour"
-  (* The syntax "@:!!" is explained in
-     https://github.com/ocaml-sf/learn-ocaml/blob/master/src/grader/test_lib.mli *)
-    (List.map (fun m -> m @:!! 2012) mois @
-     List.map (fun y -> 2 @:!! y) ans)
+  test_function_against_solution
+    (* no random cases: *)
+    ~gen:0
+    (* all exn are seen alike: *)
+    ~test:(test_eq_exn (fun _ _ -> true))
+    (* function type: *)
+    [%funty: int -> int -> int]
+    (* function name: *)
+    "nbjour"
+    (* the syntax "@:!!" is explained in test_lib.mli *)
+    (List.map (fun m -> m @:!! 2012) mois
+     @ List.map (fun y -> 2 @:!! y) ans
+     @ List.map (fun m -> m @:!! 2020) [0; 13])
 
 let () =
   set_result @@
